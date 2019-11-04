4acbb9fe46ceb20010fab2d418a24329741f901c
hittegit <hitte4896@uhcl.edu>
2019-11-04 10:59:25 -0600

    Initial Commit!

---
+++ cmake-build-debug/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/DriveController-3.cpp.o"
+  "CMakeFiles/src.dir/DriveController.cpp.o"
+  "CMakeFiles/src.dir/DropOffController.cpp.o"
+  "CMakeFiles/src.dir/LogicController.cpp.o"
+  "CMakeFiles/src.dir/ManualWaypointController.cpp.o"
+  "CMakeFiles/src.dir/ObstacleController.cpp.o"
+  "CMakeFiles/src.dir/PickUpController.cpp.o"
+  "CMakeFiles/src.dir/PID.cpp.o"
+  "CMakeFiles/src.dir/RangeController.cpp.o"
+  "CMakeFiles/src.dir/ROSAdapter-2.cpp.o"
+  "CMakeFiles/src.dir/ROSAdapter.cpp.o"
+  "CMakeFiles/src.dir/SearchController.cpp.o"
+  "CMakeFiles/src.dir/Tag.cpp.o"
+  "src.pdb"
+  "src"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
